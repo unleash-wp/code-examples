479b83a6e291594f9e31094b5e0548b83b010115
Benjamin Zekavica <info@benjamin-zekavica.de>
2025-08-21 17:53:25 +0200

    clean up

--- 6/4.sh
+++
@@ -1,7 +0,0 @@
-# This file is part of the UnleashWP Code Examples.
-# Copyright (C) 2025 Benjamin Zekavica
-#
-# Licensed under the GNU GPL v3 or later.
-# See <https://www.gnu.org/licenses/>.
-
-ddev wp media import ./wp-content/uploads/
